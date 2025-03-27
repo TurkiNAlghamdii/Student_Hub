396dec9a3077891a6b473db553d9b381158f88a0
TURKI ALGHAMDI <kiingtrkii@gmail.com>
2025-03-27 20:36:38 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Student_Hub',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  assetPrefix: '/Student_Hub/',
   /* config options here */
 };
 
